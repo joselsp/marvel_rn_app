ca5918fc6d4e0cf07cb12f8e8f65f8febe778eea
Jose Luis Solis Pino <joseluissolispino@MacBook-Air-de-Jose.local>
2018-01-06 10:47:08 +0100

    Navigation installed and tested, Empty HeroesList Screen

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Actions, Scene, Router } from 'react-native-router-flux'
+import HeroesList from 'marvel_rn_app/src/sections/HeroesList';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>App Empty</Text>
-      </View>
+      <Router>
+        <Scene key="root">
+          <Scene 
+            key={ 'HeroesList' }
+            component={ HeroesList}  
+          />
+        </Scene>
+      </Router>
     );
   }
 }
